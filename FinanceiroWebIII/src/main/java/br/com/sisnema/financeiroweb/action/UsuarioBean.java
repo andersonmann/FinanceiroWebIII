693e73716228ee905a0df1136c9783bbe4e88dab
anderson <anderson.civil@hotmail.com>
2016-05-14 17:06:11 -0300

    iniciando o desenvolvimento do modulo 5

--- FinanceiroWebIII/src/main/java/br/com/sisnema/financeiroweb/action/UsuarioBean.java
+++ FinanceiroWebIII/src/main/java/br/com/sisnema/financeiroweb/action/UsuarioBean.java
@@ -1,5 +1,7 @@
 package br.com.sisnema.financeiroweb.action;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 
@@ -12,9 +14,10 @@ import br.com.sisnema.financeiroweb.negocio.UsuarioRN;
 @ManagedBean
 @RequestScoped
 public class UsuarioBean extends ActionBean<Usuario> {
-
 	private Usuario usuario = new Usuario();
 	private String confirmaSenha;
+	private List<Usuario> lista;
+	private String destinoSalvar;
 
 	public UsuarioBean() {
 		super(new UsuarioRN());
@@ -23,6 +26,7 @@ public class UsuarioBean extends ActionBean<Usuario> {
 	public String novo() {
 		usuario = new Usuario();
 		usuario.setAtivo(true);
+		destinoSalvar = "/publico/usuarioSucesso";
 		return "usuario";
 	}
 
@@ -31,11 +35,41 @@ public class UsuarioBean extends ActionBean<Usuario> {
 			if (!StringUtils.equals(usuario.getSenha(), confirmaSenha)) {
 				apresentarMensagemDeErro("Senhas diferentes");
 				return null;
-			}			
+			}
 			boolean isInsert = (usuario.getCodigo() == null);
-			negocio.salvar(usuario);			
+			negocio.salvar(usuario);
 			apresentarMensagemDeSucesso("Usuário " + (isInsert ? "inserido" : "alterado") + "com sucesso!");
-			return "usuarioSucesso";
+			lista = null;
+			return destinoSalvar;
+		} catch (RNException e) {
+			apresentarMensagemDeErro(e);
+		}
+		return null;
+	}
+
+	public String ativar() {
+		try {
+			usuario.setAtivo(!usuario.isAtivo());
+			negocio.salvar(usuario);
+			apresentarMensagemDeSucesso("Usuário " + (usuario.isAtivo() ? "ativado" : "inativado") + "com sucesso!");
+			lista = null;
+		} catch (RNException e) {
+			apresentarMensagemDeErro(e);
+		}
+		return null;
+	}
+
+	public String editar() {
+		destinoSalvar = "/admin/usuarioAdm";
+		confirmaSenha = usuario.getSenha();
+		return "/publico/usuario";
+	}
+
+	public String excluir() {
+		try {
+			negocio.excluir(usuario);
+			apresentarMensagemDeSucesso("Usuário excluído com sucesso!");
+			lista = null;
 		} catch (RNException e) {
 			apresentarMensagemDeErro(e);
 		}
@@ -58,4 +92,22 @@ public class UsuarioBean extends ActionBean<Usuario> {
 		this.confirmaSenha = confirmaSenha;
 	}
 
+	public List<Usuario> getLista() {
+		if (lista == null) {
+			lista = negocio.pesquisar(new Usuario());
+		}
+		return lista;
+	}
+
+	public void setLista(List<Usuario> lista) {
+		this.lista = lista;
+	}
+
+	public String getDestinoSalvar() {
+		return destinoSalvar;
+	}
+
+	public void setDestinoSalvar(String destinoSalvar) {
+		this.destinoSalvar = destinoSalvar;
+	}
 }
